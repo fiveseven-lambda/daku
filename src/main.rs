cd875e435a31efde2c3ca89d81886b89978d957d
fiveseven-lambda <fiveseven.lambda@gmail.com>
2021-09-20 22:30:23 +0900

    ループ，Ctrl+D で終了

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    let mut bytes = Vec::new();
-    for i in s.chars() {
-        let len = bytes.len();
-        bytes.resize(len + i.len_utf8(), 0u8);
-        i.encode_utf8(&mut bytes[len..]);
-        bytes.push(0xe3);
-        bytes.push(0x82);
-        bytes.push(0x99);
+    loop {
+        let mut s = String::new();
+        if std::io::stdin().read_line(&mut s).unwrap() == 0 {
+            break;
+        }
+        let mut bytes = Vec::new();
+        for i in s.chars() {
+            let len = bytes.len();
+            bytes.resize(len + i.len_utf8(), 0u8);
+            i.encode_utf8(&mut bytes[len..]);
+            bytes.extend([0xe3, 0x82, 0x99]);
+        }
+        println!("{}", String::from_utf8(bytes).unwrap());
     }
-    println!("{}", String::from_utf8(bytes).unwrap());
 }
